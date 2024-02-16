8eeadfed038b67d4da21f4e7a3494b2dfa439360
Radhika <radhikasn06@gmail.com>
2024-02-16 13:13:02 +0530

    success

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Assignment2Jenkins;
+Console.WriteLine("welcome to Jenkins login page");
+LoginCredentials lc = new LoginCredentials();
+Console.WriteLine( lc.Authenticate("admin","admin@123"));
